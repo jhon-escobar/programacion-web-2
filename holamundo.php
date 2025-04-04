a428682ae3ab4b9929796c35affdc993476e1efc
jhon escobar <jhonmiltonescobarsaavedra@gamil.com>
2025-04-03 20:12:15 -0400

    mi primmer php

---
+++ holamundo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <a href="./css/php.css"></a>
+<head>
+<body>
+    
+    <?
+    $a = 5;
+    $b = 5;
+    if  ($a<$b){
+        echo "A es mayor que B "
+        else "B es menor que A"
+    }
+    ?>
+</body>
+</html>
