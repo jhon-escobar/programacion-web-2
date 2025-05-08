ed15461e37d2c5e43bb8a92870d690df61ab7cde
jhon escobar <jhonmiltonescobarsaavedra@gamil.com>
2025-05-08 14:55:03 -0400

    commit

---
+++ ejercicios clase/html/tarea.php
@@ -0,0 +1,27 @@
+<?php 
+    $fruta=$_POST['fruta'];
+    $frutaa_pellido=$_POST['fruta apellido'];
+    <
+
+
+    echo $verdura."<br>";
+    echo $verdura_apellido;
+
+    $verdura=$_POST['nombre'];
+    $verdura_apellido=$_POST['apellido'];
+
+
+    echo $verdura."<br>";
+    echo $verdura_apellido;
+
+    $cane=$_POST['nombre'];
+    $carne_apellido=$_POST['apellido'];
+
+
+    echo $carne."<br>";
+    echo $carne_apellido;
+
+
+ 
+    ?>
+    
