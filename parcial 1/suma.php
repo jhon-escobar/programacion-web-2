767e8b59a26e60bafe2622476ceefe335db7b9d2
jhon escobar <jhonmiltonescobarsaavedra@gamil.com>
2025-05-15 19:35:37 -0400

    commit

--- parcial 1/suma.php
+++ parcial 1/suma.php
@@ -1,8 +1,23 @@
+<?php
 
-    <?php 
-    ingrese el primer numero: 10
-    ingrese operador (+,-,*,/)
-    ingresr segundo numero: 5
-    el resultado de 10 + 5  es : 15
- 
-    ?>
+echo "Ingrese el primer número: ";
+$numero1 = trim(fgets(STDIN));
+
+if (!is_numeric($numero1)) {
+    echo "Error: El primer valor ingresado no es un número.\n";
+    exit(1);
+}
+
+echo "Ingrese el segundo número: ";
+$numero2 = trim(fgets(STDIN));
+
+if (!is_numeric($numero2)) {
+    echo "Error: El segundo valor ingresado no es un número.\n";
+    exit(1);
+}
+
+$suma = $numero1 + $numero2;
+
+echo "La suma de $numero1 y $numero2 es: $suma\n";
+
+?>
